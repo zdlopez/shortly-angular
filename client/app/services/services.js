43493e861f69499d8a68eb9fedf9d5cbe75605e9
zdlopez <zachary.d.lopez@gmail.com>
2015-01-17 11:59:09 -0800

    search bar implemented

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -15,16 +15,12 @@ angular.module('shortly.services', [])
     link.visits += 1;
   };
 
-  // linksFactory.addLink = function(url){
-  //   var sendLink = {url: url};
-  //   return $http({
-  //     url: '/api/links',
-  //     method: 'POST',
-  //     data:  sendLink})
-  //   .then(function(response){
-  //     return response.data;
-  //   })
-  // };
+
+
+linksFactory.isValidUrl = function(url) {
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+  return url.match(rValidUrl);
+};
 
   return linksFactory;
 })
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -5,15 +5,21 @@ angular.module('shortly.shorten', [])
   $scope.link = {};
   //console.log($location);
   //$scope.link = $location
-  // angular.extend($scope, Links);
+  angular.extend($scope, Links);
 
   $scope.addLink = function() {
-    $scope.link.url = $scope.shorten;
-    console.log($scope.shorten);
-    $http.post('/api/links', $scope.link)
-    .success(function() {
-      $location.path('/');
-    })
-    console.log($scope.link)
+    if(!$scope.isValidUrl($scope.shorten)){
+      $scope.error = 'Please enter a valid url';
+    } else {
+      $scope.error = '';
+      $scope.link.url = $scope.shorten;
+
+      console.log($scope.shorten);
+      $http.post('/api/links', $scope.link)
+      .success(function() {
+        $location.path('/');
+      })
+      console.log($scope.link)
+    }
   }
 });

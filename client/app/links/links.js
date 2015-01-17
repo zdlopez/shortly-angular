87a80b42126b3dcf9570dac3f5852a3bc312be64
zdlopez <zachary.d.lopez@gmail.com>
2015-01-16 18:54:56 -0800

    The first 3 tests pass.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,15 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, $http, Links) {
   // Your code here
+  $scope.data = {};
+
+  $scope.getLinks = function() {
+    $http.get('/api/links')
+    .then(function(response) {
+      $scope.data.links = response.data;
+    })
+  };
+
+  $scope.getLinks();
 });
